75026b97a275557e38dc90d6eb53434be01e7d0a
esponges <esponges@gmail.com>
2023-05-31 17:54:14 -0600

    refresh tokens demo

--- src/app.js
+++ src/app.js
@@ -2,13 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const router = require('./routes/routing');
-const requestLogger = require('./utilities/requestLogger');
-const errorLogger = require('./utilities/errorLogger');
 
 app.use(bodyParser.json());
-app.use(requestLogger);
 app.use('/', router);
-app.use(errorLogger);
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
--- src/routes/routing.js
+++ src/routes/routing.js
@@ -4,64 +4,68 @@ require('dotenv').config()
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const userService = require('../service/users');
 
-router.post('/register', async (req, res, next) => {
-    try {
-        let userObj = req.body;
-        let registeredUser = await userService.register(userObj);
-        res.json({ message: `${registeredUser.userName} registered successfully` })
-    } catch (err) {
-        next(err);
-    }
+let refreshTokens = [];
+
+router.post('/login', (req, res, next) => {
+    const userName = req.body.userName;
+    /* Payload - along with userName, also contains issued at time and expiry time */
+    const user = { name: userName, exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7) };
+    /* jwt.sign() creates the JSON Web Token */
+    const accessToken = jwt.sign({ name: userName, exp: Math.floor(Date.now() / 1000) + 60 }, process.env.ACCESS_TOKEN_SECRET);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    /* Adds the refresh token to an array to ensure the intruder cannot use any random token as refresh token */
+    refreshTokens.push(refreshToken);
+    res.json({ accessToken, refreshToken });
 })
 
-router.post('/login', async (req, res, next) => {
-    try {
-        const userObj = req.body;
-        const userData = await userService.login(userObj);
-        let jwtPayload = { userName: userData.userName, iat: Math.floor(Date.now() / 1000) - 60, exp: Math.floor(Date.now() / 1000) + (60 * 60) };
-        const accessToken = jwt.sign(jwtPayload, process.env.ACCESS_TOKEN_SECRET);
-        res.json({ message: `${userData.userName} logged in successfully`, accessToken: accessToken });
-    } catch (err) {
-        next(err);
+router.post("/generate-token", (req, res) => {
+    /* Fetches the refresh token from request body */
+    const refreshToken = req.body.token;
+    /* Chekcs if refresh token belongs to the user */ 
+    if (!refreshToken && !refreshTokens.includes(refreshToken)) {
+        res.status(403);
+        res.json({ message: "User not authenticated" });
     }
-})
-
-let verifyToken = (req, res, next) => {
-    let authHeader = req.headers['authorization'];
-    const accessToken = authHeader && authHeader.split(' ')[1];
-    if (!accessToken) { err.status = 401; err.message = "Invalid Request"; next(err) }
     else {
-        jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-            if (err) { err.status = 403; err.message = "Token Expired! Please Login to continue"; next(err) }
+        /* decodes and verifies the refresh token */ 
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+            if (!err) {
+                /* generates a new access token and sends the response */
+                const accessToken = jwt.sign({ username: user.name, exp: Math.floor(Date.now() / 1000) + 60 }, process.env.ACCESS_TOKEN_SECRET);
+                res.json({ accessToken });
+            } else {
+                res.status(403)
+                res.json({ message: "User not authenticated" });
+            }
+        });
+    }
+
+});
+
+
+function verifyToken(req, res, next) {
+    /* Fetches the authorization data from the request header */
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) { res.status(401); res.json({ message: "Token not available" }) }
+    else {
+        /* Verifies the token fetched from the request header */
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            /* Checks if there is any error */
+            if (err) { res.status(403); res.json({ message: "User is not Authenticated" }) }
+            /* Navigates to the next route method */
             else {
-                req.currentUser = payload;
+                req.user = user;
                 next();
             }
         })
     }
 }
 
-router.put('/place-order', verifyToken, async (req, res, next) => {
-    try {
-        let currentUser = req.currentUser;
-        let orderData = req.body;
-        let newOrderId = await userService.placeOrder(currentUser, orderData);
-        if (newOrderId) res.json({ message: `Order placed Successfully. Order Id: ${newOrderId}` });
-    } catch (err) {
-        next(err);
-    }
-})
-
-router.get('/orders', verifyToken, async (req, res, next) => {
-    try {
-        let currentUser = req.currentUser;
-        let ordersArray = await userService.getOrders(currentUser);
-        res.json(ordersArray);
-    } catch (err) {
-        next(err);
-    }
+router.get('/products', verifyToken, (req, res, next) => {
+    let user = req.user;
+    res.json({ user: user, product: { productName: 'Google Pixel 3a', price: 'USD 500' } })
 })
 
 module.exports = router;
